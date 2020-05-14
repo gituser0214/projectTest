b2a661c00ecc65db8606fdb399cf10e397c88daf
gituser0214 <64576767+gituser0214@users.noreply.github.com>
2020-05-15 02:56:38 +0900

    Create Logic.java

---
+++ src/Logic.java
@@ -0,0 +1,12 @@
+public class Logic() {
+  
+  public Logic () {
+  }
+  
+  private static fainal String EXEC_CODE_OK = "0";
+  private static fainal String EXEC_CODE_NG = "9";
+  
+  pubulic String execute() {
+    return EXEC_CODE_OK;
+  }
+}
